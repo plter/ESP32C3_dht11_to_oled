7330badfc2e9750b6f19b6758912006f4aba51bb
Peter <xtiqin@163.com>
2022-07-23 09:31:27 +0800

    Create dht11_to_oled.ino

---
+++ dht11_to_oled/dht11_to_oled.ino
@@ -0,0 +1,87 @@
+/*
+
+  PrintUTF8.ino
+  
+  Use the (Arduino compatible) u8g2 function "print"  to draw a text.
+
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list 
+    of conditions and the following disclaimer.
+    
+  * Redistributions in binary form must reproduce the above copyright notice, this 
+    list of conditions and the following disclaimer in the documentation and/or other 
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+
+*/
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <DHT.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+DHT dht(8, DHT11);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 5, 4);
+
+
+void setup(void) {
+  dht.begin();
+  
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+  u8g2.setFont(u8g2_font_wqy16_t_gb2312b);
+  u8g2.setFontDirection(0);
+}
+
+void loop(void) {
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    return;
+  }
+
+  u8g2.clearBuffer();
+  u8g2.setCursor(0, 15);
+  u8g2.print("温度: ");
+  u8g2.print(t);
+  u8g2.print("°C");
+  u8g2.setCursor(0, 40);
+  u8g2.print("湿度: ");
+  u8g2.print(h);
+  u8g2.print(" %");
+  u8g2.sendBuffer();
+
+  delay(1000);
+}
